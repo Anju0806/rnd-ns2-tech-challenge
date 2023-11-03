03d24b09949d6b18c9af00c1fe5e2f49a259be9f
anju <anjualfino@gmail.com>
2023-11-04 00:18:40 +0800

    adding new feature-  pagination

--- backend/domains/company/resolvers/index.ts
+++ backend/domains/company/resolvers/index.ts
@@ -2,11 +2,36 @@ import { CompanyService } from "../service";
 export default {
   Query: {
     async Users(parent: any, args: any, context: any, info: any): Promise<any> {
-      const { filter } = args;      
+      const { page = 1, pageSize, filter } = args;
       const _companyService = new CompanyService();
-      const data = await _companyService.getUsers(filter);
-      return { data };
-    },    
+    
+      // Fetch the total number of users (totalRecords)
+      const totalRecords = await _companyService.countUsers(filter);
+    
+      // Determine the pageSize
+      const calculatedPageSize = pageSize ? pageSize : totalRecords;
+    
+      // Calculate the start and end indexes for pagination
+      const startIndex = (page - 1) * calculatedPageSize;
+      const endIndex = page * calculatedPageSize;
+    
+      // Fetch users for the current page with pagination
+      const data = await _companyService.getUsersWithPagination(startIndex, endIndex, filter);
+    
+      // Calculate the total number of pages (totalOfPage)
+      const totalOfPage = Math.ceil(totalRecords / calculatedPageSize);
+    
+      return {
+        data,
+        meta: {
+          totalOfPage,
+          page,
+          totalOfRecord: totalRecords,
+          pageSize: calculatedPageSize,
+        },
+      };
+    },       
+        
     async Companies(parent: any, args: any, context: any, info: any): Promise<any> {
       const { filter } = args;      
       const _companyService = new CompanyService();
@@ -35,5 +60,6 @@ export default {
       const data = await _companyService.getUserCompanies({ companyIds });
       return data;
     },          
-  }  
+  },
+  
 };
